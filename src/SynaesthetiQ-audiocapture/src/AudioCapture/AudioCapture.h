c148bf9588c41871caac49cf17aab9fdf7bc35aa
ross <ross.mckeown1@gmail.com>
2023-04-17 20:26:46 +0100

    added documentation for audiocapture class;

--- src/SynaesthetiQ-audiocapture/src/AudioCapture/AudioCapture.h
+++ src/SynaesthetiQ-audiocapture/src/AudioCapture/AudioCapture.h
@@ -13,21 +13,25 @@
 #include <condition_variable>
 #include <mutex>
 
-
 #include <alsa/asoundlib.h>
 #include <fftw3.h>
 
 #include "PingPongBuffer.h"
 
-// A wrapper around the asound library
+/**
+ * @class AudioCapture
+ * @brief A wrapper around the asound library for capturing audio.
+ */
 class AudioCapture {
 public:
     typedef void (*DataAvailableCallback)(const std::vector<short>&);
+
     /**
      * @brief Constructor for this class
      *
      * Prompts user for audio device if not provided. 
      * Gets handle for audio device and sets up necessary callbacks.
+     * @param device_name The name of the audio device to capture from.
      */
     AudioCapture(std::string device_name);
 
@@ -36,6 +40,7 @@ public:
      *
      * Calls to this function is equivalent to subscribing to the data.
      * Multiple subscribers can be set up by making multiple calls.
+     * @param cb A function pointer to the callback to be registered.
      */
     void register_callback(DataAvailableCallback cb);
 
@@ -43,10 +48,9 @@ public:
      * @brief Prompts user to select audio device
      *
      * Lists all discoverable audio devices on system and prompts user to input a number selection.
+     * @return The name of the selected audio device.
      */
     std::string prompt_device_selection();
-
-
 
     // Thread, mutex, and condition variable for the capture thread
     std::thread captureThread;
@@ -54,6 +58,9 @@ public:
     std::condition_variable captureCv;
     bool captureReady;
     
+    /**
+     * @brief Destructor for this class
+     */
     ~AudioCapture();
 
 private:
@@ -63,6 +70,7 @@ private:
      *
      * This function reads the available audio data into a PingPongBuffer.
      * Optionally, it renders a visualisation of the audio data.
+     * @param pcm_callback A pointer to the ALSA asynchronous callback handler.
      */
     static void MyCallback(snd_async_handler_t* pcm_callback);
 
@@ -71,11 +79,15 @@ private:
      *
      * This function reads iterates through all callbacks registered with 'register_callback'.
      * Passes audio data from the PingPongBuffer, which is typically 4096 bytes.
+     * @param full_buffer A vector containing the audio data.
+     * @param size The number of elements in the audio data vector.
      */
-    static void call_callbacks(const std::vector<short>& full_buffer, int);
+    static void call_callbacks(const std::vector<short>& full_buffer, int size);
 
-    void CaptureThreadFunction(); // Function to run the capturing loop in a separate thread
-
+    /**
+     * @brief Function to run the capturing loop in a separate thread
+     */
+    void CaptureThreadFunction();
 
     std::ofstream audioFile;
     snd_pcm_t* handle;
